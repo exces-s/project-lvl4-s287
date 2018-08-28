ba5f66874d7d1e0b2d3b6a5b8931fe12e1f68279
Lebedev Artem <exces-st@yandex.ru>
2018-08-28 16:30:50 +0300

    add webpack config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+import koaWebpack from 'koa-webpack';
+import serve from 'koa-static';
+import Rollbar from 'rollbar';
 
 const app = new Koa();
+const router = new Router();
+const rollbar = new Rollbar('52d7eb6a0e554fdc9a59d7410b26a108');
 
-app.use(async (ctx) => {
-  ctx.body = 'Hellooooooo World';
-});
+router
+  .get('/500', (ctx, next) => {
+    ctx.body = '500 test';
+    next();
+  })
+  .get('/', (ctx, next) => {
+    ctx.body = 'World Hello';
+    next();
+  });
+
+app
+  .use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      rollbar.error(err, ctx.request);
+    }
+  })
+  .use(serve(`${__dirname}/assets`))
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+// koaWebpack('{.. options ..}')
+//   .then((middleware) => {
+//     app
+//       .use(async (ctx, next) => {
+//         try {
+//           await next();
+//         } catch (err) {
+//           rollbar.error(err, ctx.request);
+//         }
+//       })
+//       .use(middleware)
+//       .use(serve(`${__dirname}/assets`))
+//       .use(router.routes())
+//       .use(router.allowedMethods());
+//   });
 
 export default app;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/assets/index.js',
+  output: {
+    publicPath: 'dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [],
+};

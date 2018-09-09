c32bdf25dcf8cab9a4372e1541bb68a568d08846
Lebedev Artem <exces-st@yandex.ru>
2018-09-10 00:03:02 +0300

    fix production db setting

--- src/models/index.js
+++ src/models/index.js
@@ -9,8 +9,8 @@ const db = {};
 
 let sequelize;
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.DB_URI);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
